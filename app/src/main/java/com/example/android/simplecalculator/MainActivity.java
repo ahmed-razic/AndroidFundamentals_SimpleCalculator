62745b9b54417949d0c429b1f0b881243f63bbef
ahmed-razic <Ibrahim2012*>
2021-02-08 10:49:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/simplecalculator/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAdd(View view) {
+    }
+
+    public void onSub(View view) {
+    }
+
+    public void onDiv(View view) {
+    }
+
+    public void onMul(View view) {
+    }
+}
